0d6bc6e453bbdad7c7b8756dc5c3e9871a2d7346
aayush3 <112425020+aayush3@users.noreply.github.com>
2023-10-13 12:40:00 +0000

    git commit -m 'message'

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId="313131"
+let accountEmail= "aayush@mail.com"
+accountcity = "Jaipur"
+console.table([accountEmail, accountId, accountcity])
+// prefer not to use var beacause of block scope and functional scope.
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";//treat all code as per newer version oj JS.
+// alert(3+3) // We are using node js not browser.
+console.log(3+3)
+// code readability
+
+let name="Hitesh"
+let number= 19
+let isloggedIn = false
+
+// number => 2 to power 51
+// bigint 
+// string =>
+// boolean 
+// null
+// undefined
+console.log(typeof number);
+console.log(typeof null);
